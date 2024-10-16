52c0465c3f71c5d926b0c124554dcc93f53d54d8
AlexRR03 <alexciempo03@gmail.com>
2024-10-16 09:28:03 +0200

    fixed Collatz

--- src/components/Collatz.jsx
+++ src/components/Collatz.jsx
@@ -8,44 +8,41 @@ export default class Collatz extends Component {
     }
 
     obtenerNum=(e)=>{
-        e.preventDefault(e);
-        let numero=[]
+        e.preventDefault();
         let valor = parseInt(this.cajaNumero.current.value)
-        numero.push(valor)
-       while (this.state.numero == 1) {
-            if (this.state.numero%2 == 0) {
-                valor = valor/2
-                numero.push(valor)
-                
+        this.state.numero.push(valor)
+        let num2 =[]
+       while (valor!=1) {
+            if (valor%2 == 0) {
+                valor = valor/2            
             } else {
                 valor = valor*3+1
-                numero.push(valor) 
             }
-                
+              num2.push(valor)  
         }
         this.setState({
-            numero:numero
+            numero:num2
         })
 
     }
   render() {
     return (
       <div>
-        {
-            this.state.numero &&
-            (<ul>{
-                this.state.numero.map(()=>{
-                    <li>{this.state.numero}</li>
-                })
-                }</ul>)
-
-        }
         <h1>Conjetura de Collatz</h1>
         <form onSubmit={this.obtenerNum}>
             <label>Numero:</label>
             <input type="text" ref={this.cajaNumero} />
-            <button>Enviar</button>
+            <button>Conjetura Collatz</button>
         </form>
+        {
+            this.state.numero &&
+            (<ul>{
+                this.state.numero.map((num,index)=>{
+                    return(<li key={index}>{num}</li>)
+                })
+                }</ul>)
+
+        }
       </div>
     )
     }
